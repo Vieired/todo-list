de42cd04135f5f1c5dceb525f8f67a370aff3c48
Ednaldo Gomes <egomes@metatron.com.br>
2021-08-09 09:55:29 -0300

    Envia projeto - primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.models';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root></app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+
+  constructor() {
+    this.todos.push({
+      title: 'Passear com o cachorro',
+      done: false
+    });
+    this.todos.push({
+      title: 'Ir ao supermercado',
+      done: false
+    });    
+  }
+
+  addTodo() {
+
+  }
+
+  removeTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
 }
---
+++ src/models/todo.models.ts
@@ -0,0 +1,9 @@
+export class Todo {
+    /**
+     *
+     */
+    constructor(
+        public title: string,
+        public done: boolean
+    ) { }
+}

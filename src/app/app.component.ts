55ec981b63ce2caef32cfa9e46d3bc92d62b1ed9
Ednaldo Gomes <egomes@metatron.com.br>
2021-08-09 12:13:26 -0300

    feat: implementa adição e remoção de tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.models';
 
 @Component({
@@ -7,9 +8,22 @@ import { Todo } from 'src/models/todo.models';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public form: FormGroup;
   public todos: Todo[] = [];
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    })
+    
+    this.load();
+  }
+
+  load() {
     this.todos.push({
       title: 'Passear com o cachorro',
       done: false
@@ -17,15 +31,21 @@ export class AppComponent {
     this.todos.push({
       title: 'Ir ao supermercado',
       done: false
-    });    
+    }); 
   }
 
   addTodo() {
-
+    const title = this.form.controls['title'].value;
+    this.todos.push(new Todo(title, false));
+    this.form.reset();
   }
 
   removeTodo(todo: Todo) {
     const index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
   }
 }
